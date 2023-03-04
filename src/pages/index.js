77a8310058dba88270de999e4897854ec771d054
mikhacavin <mikhacavinarthur@gmail.com>
2023-03-04 10:32:34 +0700

    small update

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,12 +52,7 @@ export default function Home() {
                 />
               </li>
               <li>
-                <a
-                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="#"
-                >
-                  Resume
-                </a>
+              <button onClick={createTest} className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8">Test Data</button>
               </li>
             </ul>
           </nav>
@@ -65,7 +60,6 @@ export default function Home() {
             <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
               Eloistic Digital Corpora
             </h2>
-            <button onClick={createTest} className="btn">Create Test</button>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
               Developer and designer.
             </h3>
